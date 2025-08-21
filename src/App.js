dd0cae4393829073af369e55365ab914d9031c13
naoloui <naomi.laufer16@gmail.com>
2025-08-21 13:30:50 +0200

    Files moved from the other react app to the replacement one

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="app-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Naomi L</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
