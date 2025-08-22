d913724af2a244007f553a9b6ed47c256541a2a7
naoloui <naomi.laufer16@gmail.com>
2025-08-22 19:17:37 +0200

    Modification on h3 Meaning.js file

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,7 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="meaning">
-      <h3>{props.meaning.partofspeech}</h3>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
