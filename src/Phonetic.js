8fb9e09644b461ec26a044a388638748fb44bf00
naoloui <naomi.laufer16@gmail.com>
2025-08-23 10:20:16 +0200

    Removed br / in Phonetics.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -8,7 +8,6 @@ export default function Phonetic(props) {
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <br />
       <span className="text">{props.phonetic.text}</span>
     </div>
   );
